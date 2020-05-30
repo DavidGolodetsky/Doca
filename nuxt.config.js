08349e177ab2b479bd0b6de208867f633fb87410
David Golodetsky <davyd.lito@gmail.com>
2020-05-30 13:56:42 +0300

    changing port

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,9 @@ module.exports = {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
+  server: {
+    port: 3010,
+  },
   loading: { color: colors.blue.darken2 },
   plugins: [],
   buildModules: [
